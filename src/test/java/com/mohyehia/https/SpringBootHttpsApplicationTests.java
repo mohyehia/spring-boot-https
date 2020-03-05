56fdbcb898dadd7d975c89724260860180add832
Mohamed.Yehia <mohamed.yehia>
2020-03-05 13:22:49 +0200

    Initial commit

---
+++ src/test/java/com/mohyehia/https/SpringBootHttpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohyehia.https;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHttpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
